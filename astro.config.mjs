83122bd3a1651539f3d93d5e634585a9de866d88
Luke Fredrickson <lukeefredrickson@gmail.com>
2024-12-05 00:03:58 -0800

    Add Astro Integrations (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({ site: "https://lukefredrickson.dev" });
+export default defineConfig({
+  site: "https://lukefredrickson.dev",
+  integrations: [react(), tailwind(), sitemap(), mdx()]
+});
