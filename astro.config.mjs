ced3784d3f312e8160da6537d72e8ae38d0ebebe
Luke Fredrickson <lukeefredrickson@gmail.com>
2024-12-04 23:42:59 -0800

    `pnpm astro add sitemap`

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lukefredrickson.dev",
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), sitemap()]
 });
